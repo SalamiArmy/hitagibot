532e482359343f1e127294a9505aefdcb9a05937
SalamiArmy <ultrakiff@gmail.com>
2016-05-23 17:12:44 +0200

    cleanup tests a little

--- Tests/test_get.py
+++ Tests/test_get.py
@@ -1,3 +1,4 @@
+import configparser
 import json
 import unittest
 import plugins.get as get
@@ -6,15 +7,22 @@ import tgapi
 class TestPick(unittest.TestCase):
     def test_get_admingroup(self):
         tg = tgapi.TelegramApi
-        tg.message = json.loads("{\"message_id\":245,\"from\":{\"id\":33166369,\"first_name\":\"Ashley\",\"last_name\":\"Lewis\",\"username\":\"SalamiArmy\"},\"chat\":{\"id\":-55348600,\"title\":\"Donald Glover Appreciation\",\"type\":\"group\"},\"date\":1463933563,\"text\":\"\/get trippy swirl\",\"entities\":[{\"type\":\"bot_command\",\"offset\":0,\"length\":4}]}")
+        fullMessageText = '/get trippy swirl'
+
+        keyConfig = configparser.ConfigParser()
+        keyConfig.read(["keys.ini", "config.ini", "..\keys.ini", "..\config.ini"])
+        chatId = keyConfig['HeyBoet']['ADMIN_GROUP_CHAT_ID']
+
+        #for bot group:
+        #chatId = -1001048076684
+
+        tg.message = json.loads(
+            "{\"message_id\":245,\"from\":{\"id\":33166369,\"first_name\":\"Ashley\",\"last_name\":\"Lewis\",\"username\":\"SalamiArmy\"},\"chat\":{\"id\":" + str(
+                chatId) + ",\"title\":\"Donald Glover Appreciation\",\"type\":\"group\"},\"date\":1463933563,\"text\":\"" + fullMessageText +
+            "\",\"entities\":[{\"type\":\"bot_command\",\"offset\":0,\"length\":4}]}")
         tg.misc = json.loads("{\"bot_info\":{\"username\":\"@Bashs_Bot\"}}")
         result = get.main(tg)
         self.assertIn(result, ["jason", "justin", "mat"])
-
-    # def test_get_botgroup(self):
-    #     incomingMessage = json.loads("{\"misc\":{\"bot_info\":{\"username\":\"@Bashs_Bot\"}}, \"update_id\":709889770, \"message\":{\"message_id\":10970,\"from\":{\"id\":134480007,\"first_name\":\"Alex\",\"last_name\":\"Gaillard\",\"username\":\"Dusty_b\"},\"chat\":{\"id\":-1001048076684,\"title\":\"Jesus is my Homeboy\",\"type\":\"supergroup\"},\"date\":1463989697,\"text\":\"\/get dank memes\",\"entities\":[{\"type\":\"bot_command\",\"offset\":0,\"length\":4}]}}")
-    #     result = get.main(incomingMessage)
-    #     #self.assertIn(result, ["jason", "justin", "mat"])
 
 if __name__ == "__main__":
     unittest.main()
--- Tests/test_imgur.py
+++ Tests/test_imgur.py
@@ -1,3 +1,4 @@
+import configparser
 import json
 import unittest
 import plugins.igmur as igmur
@@ -7,12 +8,18 @@ class TestPick(unittest.TestCase):
     def test_get_admingroup(self):
         tg = tgapi.TelegramApi
         fullMessageText = '/imgur flail'
-        chatId = -55348600
+
+        keyConfig = configparser.ConfigParser()
+        keyConfig.read(["keys.ini", "config.ini", "..\keys.ini", "..\config.ini"])
+        chatId = keyConfig['HeyBoet']['ADMIN_GROUP_CHAT_ID']
 
         #for bot group:
         #chatId = -1001048076684
 
-        tg.message = json.loads("{\"message_id\":245,\"from\":{\"id\":33166369,\"first_name\":\"Ashley\",\"last_name\":\"Lewis\",\"username\":\"SalamiArmy\"},\"chat\":{\"id\":" + str(chatId) + ",\"title\":\"Donald Glover Appreciation\",\"type\":\"group\"},\"date\":1463933563,\"text\":\"" + fullMessageText + "\",\"entities\":[{\"type\":\"bot_command\",\"offset\":0,\"length\":4}]}")
+        tg.message = json.loads(
+            "{\"message_id\":245,\"from\":{\"id\":33166369,\"first_name\":\"Ashley\",\"last_name\":\"Lewis\",\"username\":\"SalamiArmy\"},\"chat\":{\"id\":" + str(
+                chatId) + ",\"title\":\"Donald Glover Appreciation\",\"type\":\"group\"},\"date\":1463933563,\"text\":\"" + fullMessageText +
+            "\",\"entities\":[{\"type\":\"bot_command\",\"offset\":0,\"length\":4}]}")
         tg.misc = json.loads("{\"bot_info\":{\"username\":\"@Bashs_Bot\"}}")
         result = igmur.main(tg)
         self.assertIn(result, ["jason", "justin", "mat"])
